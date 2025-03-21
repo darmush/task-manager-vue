ef2cbd304858ce8234912cdb7f283b0fc5b6c3d6
Darya Podskrebalina <2012dr2012@gmail.com>
2025-03-21 14:27:00 +0400

    feat: add edit tags view

---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <Overlay @cancel="$emit('cancel')">
+        <div
+             class="max-w-96 z-30 flex flex-col items-center justify-centers p-6 gap-5 bg-white shadow-md border border-line-color rounded-card-rounded ">
+            <div class="flex flex-col gap-2">
+                <h3 class="text-base font-semibold">
+                    <slot name="title"></slot>
+                </h3>
+                <div class="text-gray-text">
+                    <slot name="text"></slot>
+                </div>
+            </div>
+            <div class="flex flex-row self-end gap-2">
+                <Button text="Cancel"
+                        :style="'white'"
+                        type="button"
+                        @click="$emit('cancel')"></Button>
+                <slot name="buttonAction"></slot>
+            </div>
+        </div>
+    </Overlay>
+</template>
+
+<script setup>
+import Overlay from './Overlay.vue';
+import Button from './Button.vue';
+defineEmits(['cancel']);
+</script>
---
+++ src/components/icons/DeleteIcon.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg class="stroke-gray-text-light stroke-icon-width w-5 h-5"
+         width="18"
+         height="20"
+         viewBox="0 0 18 20"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.5 5.00002H16.5M14.8333 5.00002V16.6667C14.8333 17.5 14 18.3334 13.1667 18.3334H4.83333C4 18.3334 3.16667 17.5 3.16667 16.6667V5.00002M5.66667 5.00002V3.33335C5.66667 2.50002 6.5 1.66669 7.33333 1.66669H10.6667C11.5 1.66669 12.3333 2.50002 12.3333 3.33335V5.00002M7.33333 9.16669V14.1667M10.6667 9.16669V14.1667"
+              stroke-linecap="round"
+              stroke-linejoin="round" />
+    </svg>
+</template>
